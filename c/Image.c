269864f416a6a6cfe80b296ede41b3cf50647e6a
momus2000 <muhammadmustafa4000@gmail.com>
2021-11-21 03:37:53 -0500

    Image in C

--- c/Image.c
+++ c/Image.c
@@ -1,15 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define WIDTH 10
+#define HEIGHT 10
+
 struct Image{
-	int width;
-	int height;
-	int pixels[width][height];
-}
+	int (*pixels)[HEIGHT];
+};
 
 
 void set_pixel(struct Image *image, int i, int j, int color){
-	image[i][j] = color;
+	image->pixels[i][j] = color;
 }
 
 void write_ppm(struct Image *image){
-	
+	int width = WIDTH;
+	int height = HEIGHT;
+
+	FILE *ptr;
+
+	ptr = fopen("/Users/a./Desktop/ray/ray-tracer/c/test.ppm", "w");
+
+	if(ptr == NULL){
+		exit(1);
+	}
+
+	for(int i=0;i<width;i++){
+		for(int j=0;j<height;j++){
+			fprintf(ptr,"%d",image->pixels[i][j]);
+		}
+	}
+	fclose(ptr);
+}
+
+int main(int argc, char const *argv[]){
+	int pixels[WIDTH][HEIGHT] = {{1}};
+	struct Image *img = malloc(sizeof(struct Image));
+	img->pixels = pixels;
+	write_ppm(img);
+	// struct Image = {co, 5, pixels}
 }
 
