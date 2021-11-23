05e153a67f75d98290ca58baa02aead639f7e5a6
momus2000 <muhammadmustafa4000@gmail.com>
2021-11-23 11:06:01 -0500

    Checkered Material

--- c/Material.h
+++ c/Material.h
@@ -8,3 +8,15 @@ struct Material{
 struct Vector material_color_at(struct Material mt){
 	return mt.color;
 }
+
+struct Checkered_Material{
+	struct Vector color1;
+	struct Vector color2;
+	struct Vector ambient;
+	struct Vector diffuse;
+	struct Vector specular;
+}
+
+struct Vector checkered_material_color_at(struct Checkered_Material mt){
+	return mt.color;
+}
