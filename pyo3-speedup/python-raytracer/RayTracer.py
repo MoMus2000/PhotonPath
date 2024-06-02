4bc953669d76f33894a16a5b48c1fd8e354eb050
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-06-02 08:33:20 -0400

    Add pyo3 speedup

--- pyo3-speedup/python-raytracer/RayTracer.py
+++ pyo3-speedup/python-raytracer/RayTracer.py
@@ -1,4 +1,30 @@
+import math
+from Ray import Ray
 import raytracer_rs
+
+
+def closest_object_index(intersections):
+    min_index = -1
+
+    if len(intersections) == 0:
+        return min_index
+    elif len(intersections) == 1:
+        if intersections[0] > 0:
+            min_index = 0
+    else:
+        max_val = 0
+        for i in range(0, len(intersections)):
+            if max_val < intersections[i]:
+                max_val = intersections[i]
+
+        if max_val > 0:
+            for i in range(0, len(intersections)):
+                if 0 < intersections[i] <= max_val:
+                    max_val = intersections[i]
+                    min_index = i
+
+    return min_index
+
 
 def color_at(intersect_pos, intersect_ray_direction, scene_objects,
              closest_obj_index, lights, accuracy, ambient):
