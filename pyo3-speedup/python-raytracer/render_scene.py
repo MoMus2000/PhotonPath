7a140f2c1fbafe53a2e26edc29fc611a3935300d
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-06-07 17:37:55 -0400

    video

--- pyo3-speedup/python-raytracer/render_scene.py
+++ pyo3-speedup/python-raytracer/render_scene.py
@@ -13,8 +13,8 @@ import copy
 start = time.time()
 print("TRACIN' DEM RAYS...")
 
-width = int(640)
-height = int(480)
+width = int(640*2)
+height = int(480*2)
 
 origin = Vector(0, 0, 0)
 unit_x = Vector(1, 0, 0)
@@ -57,6 +57,8 @@ light2 = Light(Vector(5, 5, 5), yellow_light)
 lights = [light, light2]
 
 sphere = Sphere(origin, 1, green)
+# sphere_t = Sphere(Vector(3, 0, -3), 1, green)
+sphere_t = Sphere(Vector(0, 0, -0), 1, green)
 sphere2 = Sphere(sphere_center, 0.5, maroon)
 sphere3 = Sphere(Vector(0, 2, 0), 1, blue)
 plane = Plane(unit_y, -1, tile_floor)
@@ -69,12 +71,18 @@ triangle4 = Triangle(Vector(-3, -1, 3), Vector(-3, 4, 3), Vector(-3, 4, -3), ora
 
 scene_objects = [
     Scene(None, None, sphere), 
+    Scene(None, None, sphere_t), 
     Scene(None, None, sphere2),
     Scene(None, None, sphere3),
     Scene(None, plane, None),
 ]
 
-
+# start 3, 0, -3
+# for i in range(2, 20):
+    #sphere_t = Sphere(Vector(3, 0, -3), 1, green)
+    # x, z = 3*i, -3*i
+    # sphere = Scene(None, None, Sphere(Vector(x, 0, -z), 1, green))
+    # scene_objects.append(sphere)
 
 image = Image.new("RGB", (width, height))
 
@@ -82,48 +90,52 @@ o_count = 0
 
 render_objects = []
 
-for i in range(0, 2):
+for i in range(0, 30):
     sub = np.linspace(i, i+1, 120)
     for idx, val in enumerate(sub):
         renderer = RenderScene(scene_objects,camera,lights, width, height, ambient, accuracy)
         render_objects.append(renderer)
-        if i <=5:
-            cam_position = Vector(5*val, 2.5, val*5)
+        cam_position = Vector(5*val, 2.5, val*5)
         camera = Camera(cam_position, cam_direction, cam_right, cam_down)
+        sphere = scene_objects[1].sphere
+        sphere.center = Vector(val*4.45, 0, val*4.45)
+        scene_objects[1] = Scene(None, None, sphere)
 
         o_count += 1
 
+    light = Light(Vector(val, val, val), yellow_light)
+    lights.append(light)
 
-for i in range(2, 1, -1):
-    sub = np.linspace(i, i-1, 120)
-    for idx, val in enumerate(sub):
-        renderer = RenderScene(scene_objects,camera,lights, width, height, ambient, accuracy)
-        render_objects.append(renderer)
-        if i <=5:
-            cam_position = Vector(5*val, 2.5, val*5)
-        camera = Camera(cam_position, cam_direction, cam_right, cam_down)
 
-        o_count += 1
+# for i in range(20, 1, -1):
+#     sub = np.linspace(i, i-1, 120)
+#     for idx, val in enumerate(sub):
+#         renderer = RenderScene(scene_objects,camera,lights, width, height, ambient, accuracy)
+#         render_objects.append(renderer)
+#         cam_position = Vector(5*val, 2.5*val, val*5)
+#         camera = Camera(cam_position, cam_direction, cam_right, cam_down)
 
-for i in range(0, 2):
-    sub = np.linspace(i, i+1, 120)
-    for idx, val in enumerate(sub):
-        renderer = RenderScene(scene_objects,camera,lights, width, height, ambient, accuracy)
-        render_objects.append(renderer)
+#         o_count += 1
 
-        cam_position = Vector(5*val, 2.5, 2)
-        camera = Camera(cam_position, cam_direction, cam_right, cam_down)
+# for i in range(0, 2):
+#     sub = np.linspace(i, i+1, 120)
+#     for idx, val in enumerate(sub):
+#         renderer = RenderScene(scene_objects,camera,lights, width, height, ambient, accuracy)
+#         render_objects.append(renderer)
 
-        o_count += 1
+#         cam_position = Vector(5*val, 2.5, 2)
+#         camera = Camera(cam_position, cam_direction, cam_right, cam_down)
 
-for i in range(2, 0, -1):
-    sub = np.linspace(i, i-1, 120)
-    for idx, val in enumerate(sub):
-        renderer = RenderScene(scene_objects,camera,lights, width, height, ambient, accuracy)
-        render_objects.append(renderer)
-        cam_position = Vector(5*val, 2.5, 2)
-        camera = Camera(cam_position, cam_direction, cam_right, cam_down)
+#         o_count += 1
 
-        o_count += 1
+# for i in range(2, 0, -1):
+#     sub = np.linspace(i, i-1, 120)
+#     for idx, val in enumerate(sub):
+#         renderer = RenderScene(scene_objects,camera,lights, width, height, ambient, accuracy)
+#         render_objects.append(renderer)
+#         cam_position = Vector(5*val, 2.5, 2)
+#         camera = Camera(cam_position, cam_direction, cam_right, cam_down)
+
+#         o_count += 1
 
 render_objects[0].par_render(render_objects, width, height)
