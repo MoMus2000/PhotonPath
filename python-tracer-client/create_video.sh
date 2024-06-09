8ff5d4a31c7862cd818d5e0e56c46ea64e32693c
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-06-09 00:41:19 -0400

    Rename folder

--- python-tracer-client/create_video.sh
+++ python-tracer-client/create_video.sh
@@ -5,7 +5,7 @@ python3 video.py
 
 echo "Deleting images"
 
-cd /Users/mmuhammad/Desktop/projects/ray-tracer/pyo3-speedup/python-raytracer/images
+cd /Users/mmuhammad/Desktop/projects/ray-tracer/python-tracer-client/images
 rm *
 
 echo "Done"
